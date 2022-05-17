e1e1021c2e9ad4b53da1874613e09d6f5a32dee5
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-05-17 07:43:52 +0530

    Added:Creating Employee PayRoll Service Request

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee PayRoll App Problem");
+		SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
+
+}
