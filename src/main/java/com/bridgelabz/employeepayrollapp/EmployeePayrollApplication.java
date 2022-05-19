1e13388819273ce5a4d7cea8c1e4618e61fd724a
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-05-19 07:06:54 +0530

    Added:Use Lombok Library for Logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,14 +1,25 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.hibernate.validator.internal.util.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+Use Lombok Library for Logging i.e Slf4j
+ */
+
 @SpringBootApplication
+@Slf4j
+
 public class EmployeePayrollApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee PayRoll App Problem");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll App !!!!!");
+        SpringApplication.run(EmployeePayrollApplication.class, args);
+        Log.info("employee payroll app started");
+    }
 
 }
+
