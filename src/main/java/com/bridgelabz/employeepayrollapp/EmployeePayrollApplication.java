9d5509f773f0e45d8facf2dcd3c9f78765bb6018
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-05-19 07:49:18 +0530

    Added:Setting Up Dev And Production Environment

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,25 +1,20 @@
 package com.bridgelabz.employeepayrollapp;
 
-import lombok.extern.slf4j.Slf4j;
-
-import org.hibernate.validator.internal.util.logging.Log;
+import org.apache.juli.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-/**
-Use Lombok Library for Logging i.e Slf4j
- */
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-
 public class EmployeePayrollApplication {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Payroll App !!!!!");
-        SpringApplication.run(EmployeePayrollApplication.class, args);
-        Log.info("employee payroll app started");
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll App !!!!!");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		Log.info("Employee Payroll App Started in {} Environment");
+	}
 
 }
-
